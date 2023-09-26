585c3c6fae226a51e087ee8ed9acce2b33cac51b
LpHiX <martin.leung.ht@gmail.com>
2023-09-26 20:56:25 +0100

    i hate you
    i hate you pc for deleting EVERY CHANGES I MADE

--- .Old/draft1/README.md
+++
@@ -1,2 +0,0 @@
-# canardboard
-Canard board
--- Firmware/src/nrcgeddan/nrcgeddan.cpp
+++ Firmware/src/nrcgeddan/nrcgeddan.cpp
@@ -4,16 +4,18 @@
 #include <libriccore/commands/commandhandler.h>
 #include "config/services_config.h"
 #include <librrc/Helpers/nvsstore.h>
+#include "librrc/Helpers/rangemap.h"
+
 
 void NRCGeddan::setup()
 {
+    
     geddanServo1.setup();
     geddanServo2.setup();
     geddanServo3.setup();
     prevLogMessageTime = millis();
 }
-
-void NRCGeddan::allGotoDesiredAngle(float desiredAngle, bool unlimited = false) // -15 to 15
+void NRCGeddan::allGotoCalibratedAngle(float desiredAngle, bool unlimited) // -15 to 15
 {
     if(!unlimited){
         if(desiredAngle > 15){
@@ -22,16 +24,22 @@ void NRCGeddan::allGotoDesiredAngle(float desiredAngle, bool unlimited = false) 
             desiredAngle = -15;
         }
     }
-    geddanServo1.goto_Angle(desiredAngle + _default_angle1);
-    geddanServo2.goto_Angle(desiredAngle + _default_angle2);
-    geddanServo3.goto_Angle(desiredAngle + _default_angle3);
+    geddanServo1.goto_AngleHighRes(desiredAngle + _default_angle1);
+    geddanServo2.goto_AngleHighRes(desiredAngle + _default_angle2);
+    geddanServo3.goto_AngleHighRes(desiredAngle + _default_angle3);
+}
+void NRCGeddan::allGotoRawAngle(float angle)
+{
+    geddanServo1.goto_AngleHighRes(angle);
+    geddanServo2.goto_AngleHighRes(angle);
+    geddanServo3.goto_AngleHighRes(angle);
 }
 
 void NRCGeddan::loadCalibration(){
     
     std::string NVSName = "Srvo1" + std::to_string(_geddanServo1Channel);
-    std::string NVSName = "Srvo2" + std::to_string(_geddanServo2Channel);
-    std::string NVSName = "Srvo3" + std::to_string(_geddanServo3Channel);
+    //std::string NVSName = "Srvo2" + std::to_string(_geddanServo2Channel);
+    //std::string NVSName = "Srvo3" + std::to_string(_geddanServo3Channel);
     NVSStore _NVS(NVSName, NVSStore::calibrationType::Servo);
     
 
@@ -47,58 +55,86 @@ void NRCGeddan::loadCalibration(){
 }
 
 
-
 void NRCGeddan::update()
 {
     switch(currentGeddanState){
-        case GeddanState::Startup:
+        case GeddanState::ConstantRoll:
+        {
+            _imu.update(imustats);
+            _zRollRate = imustats.gz;
+
+
+            
+
+            error = _targetRollRate - _zRollRate;
+            allGotoCalibratedAngle(- error * Kp);
+            
+            break;
+        }
+        case GeddanState::HoldZero:
+        {
+            allGotoCalibratedAngle(0);
+            break;
+        }
+        case GeddanState::WiggleTest:
         {
             if (timeFrameCheck(zeroCanards, startSlowSpinLeft))
             {
-                allGotoDesiredAngle(0);
+                allGotoRawAngle(90);
             }
-            else if (timeFrameCheck(startSlowSpinLeft, startSlowSpinRight))
+            else if (timeFrameCheck(startSlowSpinLeft, zeroCanards2))
             {
-                //  TODO: Make this go slower
-                geddanServo1.goto_AngleHighRes(0);
-                geddanServo2.goto_AngleHighRes(0);
-                geddanServo3.goto_AngleHighRes(0);
+                allGotoRawAngle(LIBRRC::rangemap<float>(millis() - wiggleTestTime, startSlowSpinLeft, startSlowSpinLeft + 800, 90, 0));
             }
-            else if (timeFrameCheck(startSlowSpinRight, startSlowSpinRight))
+            else if (timeFrameCheck(zeroCanards2, startSlowSpinRight))
             {
-                //  TODO: Make this go slower
-                geddanServo1.goto_AngleHighRes(180);
-                geddanServo2.goto_AngleHighRes(180);
-                geddanServo3.goto_AngleHighRes(180);
+                allGotoRawAngle(LIBRRC::rangemap<float>(millis() - wiggleTestTime, zeroCanards2, zeroCanards2 + 800, 0, 90));
             }
-
-            
-            geddanServo1.goto_Angle(0);
-            geddanServo2.goto_Angle(0);
-            geddanServo3.goto_Angle(0);
+            else if (timeFrameCheck(startSlowSpinRight, zeroCanards3))
+            {
+                allGotoRawAngle(LIBRRC::rangemap<float>(millis() - wiggleTestTime, startSlowSpinRight, startSlowSpinLeft + 800, 90, 180));
+            }
+            else if (timeFrameCheck(zeroCanards3, zeroCanards4))
+            {
+                allGotoRawAngle(LIBRRC::rangemap<float>(millis() - wiggleTestTime, zeroCanards3, zeroCanards3 + 800, 180, 90));
+            }
+            else if (timeFrameCheck(zeroCanards4, startSpinLeft))
+            {
+                allGotoCalibratedAngle(0);
+            }
+            else if (timeFrameCheck(startSpinLeft, startSpinRight))
+            {
+                allGotoCalibratedAngle(-15);
+            }
+            else if (timeFrameCheck(startSpinRight, endOfWiggleSeq))
+            {
+                allGotoCalibratedAngle(15);
+            }
+            else if (timeFrameCheck(endOfWiggleSeq))
+            {
+                allGotoCalibratedAngle(0);
+                currentGeddanState = previousGeddanState;
+            }
             break;
         }
-        case GeddanState::Shutdown:
+        case GeddanState::Debug:
         {
-            geddanServo1.goto_Angle(0);
-            geddanServo2.goto_Angle(0);
-            geddanServo3.goto_Angle(0);
-            break
+            break;
         }
-        case GeddanState::MotorIdle:
+        case GeddanState::Fun:
         {
-
+            break;
         }
     }
 }
 
 bool NRCGeddan::timeFrameCheck(int64_t start_time, int64_t end_time)
 {
-    if (millis() - ignitionTime > start_time && end_time == -1){
+    if (millis() - wiggleTestTime > start_time && end_time == -1){
         return true;
     }
     
-    else if (millis() - ignitionTime > start_time && millis() - ignitionTime < end_time){
+    else if (millis() - wiggleTestTime > start_time && millis() - wiggleTestTime < end_time){
         return true;
     }
 
@@ -110,35 +146,47 @@ bool NRCGeddan::timeFrameCheck(int64_t start_time, int64_t end_time)
 void NRCGeddan::execute_impl(packetptr_t packetptr) //Set it to roll rate, mili radians per second
 {
     SimpleCommandPacket execute_command(*packetptr);
+    _targetRollRate = execute_command.arg;
+}
 
-    switch (execute_command.arg)
+void NRCGeddan::extendedCommandHandler_impl(const NRCPacket::NRC_COMMAND_ID commandID,packetptr_t packetptr){
+    SimpleCommandPacket command_packet(*packetptr);
+    switch(static_cast<uint8_t>(commandID))
     {
-    case 1:
-    {
-        if (currentGeddanState != GeddanState::)
+        case 5:
         {
+            currentGeddanState = static_cast<GeddanState>(command_packet.arg);
+        }
+        case 7: // Debug go to calibrated angle
+        {
+            if(currentGeddanState == GeddanState::Debug){
+                allGotoCalibratedAngle(command_packet.arg);
+                break;
+            }
+            else{
+                break;
+            }
+        }
+        case 8: // Debug go to raw angle
+        {
+            if(currentGeddanState == GeddanState::Debug){
+                allGotoRawAngle(command_packet.arg);
+                break;
+            }
+            else{
+                break;
+            }
+        }
+        case 6: // Wiggle test
+        {
+            previousGeddanState = currentGeddanState;
+            currentGeddanState = GeddanState::WiggleTest;
+            wiggleTestTime = millis();
+        }
+        default:
+        {
+            NRCRemoteActuatorBase::extendedCommandHandler_impl(commandID,std::move(packetptr));
             break;
         }
-        currentEngineState = EngineState::Ignition;
-        ignitionTime = millis();
-        RicCoreLogging::log<RicCoreLoggingConfig::LOGGERS::SYS>("Ignition");
-        break;
-    }
-    case 2:
-    {
-        currentEngineState = EngineState::ShutDown;
-        RicCoreLogging::log<RicCoreLoggingConfig::LOGGERS::SYS>("ShutDown");
-        break;
-    }
-    case 3:
-    {
-        if (currentEngineState != EngineState::ShutDown)
-        {
-            break;
-        }
-        currentEngineState = EngineState::Debug;
-        RicCoreLogging::log<RicCoreLoggingConfig::LOGGERS::SYS>("Entered debug");
-        break;
-    }
     }
 }
